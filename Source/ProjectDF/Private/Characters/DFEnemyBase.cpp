d91f974dc9b6c25e50ecc3fa0fb598964c601fd8
humilshin <104838481+humilshin@users.noreply.github.com>
2025-11-14 06:49:14 +0900

    comments

--- Source/ProjectDF/Private/Characters/DFEnemyBase.cpp
+++ Source/ProjectDF/Private/Characters/DFEnemyBase.cpp
@@ -5,6 +5,7 @@
 #include "Characters/DFHeroCharacter.h"
 #include "Components/PlayerCurrencyComponent.h"
 
+// Enemy Death Handle : Base를 통해 공통 기능 구현
 void ADFEnemyBase::HandleDeath(AActor* Killer)
 {
 	if (ADFHeroCharacter* Hero = Cast<ADFHeroCharacter>(Killer))
