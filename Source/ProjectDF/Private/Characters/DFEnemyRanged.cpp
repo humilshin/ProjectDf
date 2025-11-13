37d1e1d458743303691a170d855682aa9d4819d9
humilshin <104838481+humilshin@users.noreply.github.com>
2025-11-14 07:03:36 +0900

    comments

--- Source/ProjectDF/Private/Characters/DFEnemyRanged.cpp
+++ Source/ProjectDF/Private/Characters/DFEnemyRanged.cpp
@@ -1,6 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Characters/DFEnemyRanged.h"
 #include "Projectile.h"
 #include "Kismet/GameplayStatics.h"
@@ -11,24 +8,29 @@ ADFEnemyRanged::ADFEnemyRanged()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	// 투사체 스폰 포인트
 	ProjectileSpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("Spawn Point"));
 	ProjectileSpawnPoint->SetupAttachment(RootComponent);
-	
+
+	// 이동 속도 및 RVO 회피 설정
 	if (UCharacterMovementComponent* MoveComp = GetCharacterMovement())
 	{
 		MoveComp->MaxWalkSpeed = Speed;
-
-		//RVO 회피 구현
 		MoveComp->bUseRVOAvoidance = true;
-		MoveComp->AvoidanceConsiderationRadius = 100.f; // 서로 인식 거리
-		MoveComp->AvoidanceWeight = 0.5f;        
+		MoveComp->AvoidanceConsiderationRadius = 100.f;
+		MoveComp->AvoidanceWeight = 0.5f;
 	}
 }
 
+/* 
+	매 프레임 주변 원거리 적과 거리 유지 이동
+	-> RVO Avoidance를 이용하기 위해 작은 움직임을 부여
+*/
 void ADFEnemyRanged::Tick(float DeltaTime)
 {
 	TArray<AActor*> Overlaps;
 	GetOverlappingActors(Overlaps, ADFEnemyRanged::StaticClass());
+
 	for (AActor* Other : Overlaps)
 	{
 		if (Other == this) continue;
@@ -41,15 +43,14 @@ void ADFEnemyRanged::Tick(float DeltaTime)
 	}
 }
 
+// 초기 설정 및 공격 루프 시작
 void ADFEnemyRanged::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// Hero 캐시
 	Hero = UGameplayStatics::GetPlayerPawn(this, 0);
 	if (!Hero) return;
 
-	// 주기적으로 발사
 	GetWorldTimerManager().SetTimer(
 		FireTimerHandle,
 		this,
@@ -60,24 +61,21 @@ void ADFEnemyRanged::BeginPlay()
 	);
 }
 
+// 투사체 발사
 void ADFEnemyRanged::FireProjectile()
 {
 	if (!Hero || !ProjectileClass) return;
 
-	// Hero 방향으로 회전
 	FaceHero();
-	
+
 	const FVector MyLocation = ProjectileSpawnPoint->GetComponentLocation();
 	const FVector TargetLocation = Hero->GetActorLocation();
 	const FVector TempDirection = (TargetLocation - MyLocation).GetSafeNormal();
 	const FVector Direction = FVector(TempDirection.X, TempDirection.Y, 0.0f);
 
-	// Hero가 너무 멀면 발사 X
 	const float Distance = FVector::Dist(MyLocation, TargetLocation);
-	if (Distance > FireRange)
-		return;
+	if (Distance > FireRange) return;
 
-	// 투사체 생성
 	const FVector SpawnLocation = ProjectileSpawnPoint->GetComponentLocation();
 	const FRotator SpawnRotation = Direction.Rotation();
 
@@ -94,6 +92,7 @@ void ADFEnemyRanged::FireProjectile()
 	}
 }
 
+// 플레이어 바라보도록 회전
 void ADFEnemyRanged::FaceHero()
 {
 	if (!Hero) return;
@@ -102,6 +101,6 @@ void ADFEnemyRanged::FaceHero()
 	FRotator LookAtRot = FRotationMatrix::MakeFromX(ToTarget).Rotator();
 	LookAtRot.Pitch = 0.f;
 	LookAtRot.Roll = 0.f;
+
 	SetActorRotation(LookAtRot);
 }
-
